c917081dc78700934d63815bfc144beae95e7310
ezratrotter <ezratrotter@gmail.com>
2021-02-07 15:15:55 +0100

    working on tests

--- train_model.py
+++ train_model.py
@@ -14,229 +14,58 @@ import LOCAL_VARS
 import make_crossline as mkcross
 import wall_score
 from scipy import signal
-
-
-gdf = gpd.read_file('data/3D_cross_sections.geojson')
-
-##%
-# %%
-# sbst_gdf = gdf[:500]
-
-# for _,row in sbst_gdf.iterrows():
-#    elevs = [p.z for p in row['geometry']]
-#    average = sum(elevs) / len(elevs)
-#    nrml_elevs = [(p - average) for p in elevs]
-
-#    x = np.arange(len(nrml_elevs))
-#    y = np.array(nrml_elevs)
-
-#    peaks = signal.find_peaks(y, height=0.2)
-#    print('---peaks----')
-#    print(peaks)
-
-#    plt.figure()
-#    plt.scatter(x,y)
-#    plt.yticks(np.arange(-0.6,.6, step=0.1))
-   plt.show()
-
-# scipy.signal.find_peaks(x, height=None, threshold=None, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
-
-
-
-# %%
-from osgeo import gdal, ogr
-import os
-import math
-import numpy as np
-import geopandas as gpd
-from osgeo import gdal, ogr
-import sys, math
-import numpy as np
-from shapely.geometry import Point, LineString, MultiPoint
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import LOCAL_VARS
-import make_crossline as mkcross
-import wall_score
-from scipy import signal
-
-
-gdf = gpd.read_file('data/3D_cross_sections.geojson')
-#%%
 from shapely import affinity
 import scipy
 from sklearn import linear_model as LinearRegression
 from sklearn.utils.extmath import safe_sparse_dot
 
+
+gdf = gpd.read_file('data/3D_cross_sections.geojson')
+
+#%%
+def earthwall_test(geom):
+   ##elevations
+   z = [p.z for p in geom]
+   ## steps in x direction
+   x = list(np.arange(0, 20.01, step=0.4))
+
+   ## make x, z coords
+   coords = []
+   for i in range(len(z)):
+      coord = (x[i], z[i])
+      coords.append(coord)
+
+   wall_score.only_plot(MultiPoint(coords), 'original', 'green')
+
+   #### calculate rotations and transformation
+   rise, intercept = wall_score.linear_regression_score3D(geom)
+
+   origin = (0,intercept)
+
+   run = 0.4
+
+   rotation = wall_score.rise_run_to_angle(rise, run)
+
+   new_geom = affinity.rotate(MultiPoint(coords), -rotation, origin = origin)
+
+   wall_score.only_plot(new_geom, 'rotate', 'red')
+   # new_x = np.array([p.x for p in new_geom])
+   # new_y = np.array([p.y for p in new_geom])
+
+   return True
+
 sub_gdf = gdf[
     :200]
 
-
-def linear_regression_score3D(multipoint):
-
-   elevs = [p.z for p in multipoint]
-
-   x = np.arange(len(elevs))
-   y = np.array(elevs)
-
-   xr = x.reshape(-1, 1)
-
-   LR = LinearRegression.LinearRegression()
-   LR.fit(xr, y)
-
-   score = LR.score(xr, y)
-
-   prediction = LR.predict(xr)
-
-   # return safe_sparse_dot(xr, LR.coef_.T, dense_output=True) + LR.intercept_
-   return (LR.coef_, LR.intercept_)
-
-### uses math 
-def pnt_from_rtn_arnd_orgn(point, origin, angle):
-
-   p = {'x': point[0], 'y':point[1]}
-   o = {'x': origin[0], 'y':origin[1]}
-
-   # if (p['x'] == o['x'] & p['y'] == o['y']): return point
-
-   cos = math.cos(angle)
-   sin = math.sin(angle)
-
-   dx = p['x'] - o['x']
-   dy = p['y'] - o['y']
-
-   nx = (cos * dx) + (sin * dy) + o['x']
-   ny = (cos * dy) - (sin * dx) + o['y']
-
-   return [nx, ny]
-
-
-def rise_run_to_angle(rise, run):
-   return math.degrees(math.atan(rise/run))
+ids = []
+geometries = []
+types = []
 
 for _, row in sub_gdf.iterrows():
+
+   obj_id = row['OBJECTID']
    geometry = row['geometry']
-   elevs = [x.z for x in geometry]
-   steps = list(np.arange(0, 20.01, step=0.4))
-   profile_coords = []
-   for i in range(len(elevs)):
-      pair = (steps[i], elevs[i])
-      profile_coords.append(pair)
-   # scipy.signal.find_peaks(x, height=None, threshold=None, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
-   peaks, _ = scipy.signal.find_peaks(elevs, prominence=0.17)
-
-   if len(peaks) != 1: wall_score.plot(geometry)
-
-   #### calculate rotations and transformation
-   formula = linear_regression_score3D(geometry)
-   
-   rise = formula[0][0]
-   step = 0.4
-
-   rotation = rise_run_to_angle(rise, step)
+   earthwall_test(geometry)
   
-   new_geom = affinity.rotate(MultiPoint(profile_coords), -rotation, origin = origin)
-
-   new_x = np.array([p.x for p in new_geom])
-   new_y = np.array([p.y for p in new_geom])
-  
-   if len(peaks) != 1: 
-      print(len(peaks))
-      plt.figure()
-      plt.scatter(new_x, new_y)
-
-      LR1 = LinearRegression.LinearRegression()
-
-      new_xr = new_x.reshape(-1, 1)
-
-      LR1.fit(new_xr, new_y)
-      prediction = LR1.predict(new_xr)
-      plt.plot(new_xr, prediction)
-      plt.show()
-      
-      
 
 # %%
-
-# from osgeo import gdal, ogr
-# import os
-# import math
-# import numpy as np
-# import geopandas as gpd
-# from osgeo import gdal, ogr
-# import sys, math
-# import numpy as np
-# from shapely.geometry import Point, LineString, MultiPoint
-# import geopandas as gpd
-# import matplotlib.pyplot as plt
-# import LOCAL_VARS
-# import make_crossline as mkcross
-# import wall_score
-# from scipy import signal
-
-# import scipy
-# from sklearn import linear_model as LinearRegression
-# from sklearn.utils.extmath import safe_sparse_dot
-
-# from shapely import affinity
-
-
-# x = np.array([0,1, 2, 3, 4, 5])
-# y = np.array([0,1, 2, 3, 4, 5])
-# plt.figure()
-# plt.scatter(x, y)
-
-# coords = []
-# for i in range(len(y)):
-#    pair = (x[i], y[i])
-#    coords.append(pair)
-
-# xr = x.reshape(-1, 1)
-
-# LR = LinearRegression.LinearRegression()
-# LR.fit(xr, y)
-
-#    #### calculate rotations and transformation
-# formula = (LR.coef_, LR.intercept_)
- 
-# print(formula)
-# origin = (0, formula[1])
-# point2 = (step, formula[1] + (step * formula[0][0]))
-# rotation = angle(origin, point2)
-
-# print(origin, point2)
-
-# # new_elevs = [pnt_from_rtn_arnd_orgn(x, origin, rotation) for x in coords]
-# my_geom = MultiPoint(coords)
-# rotation = math.degrees(rotation)
-# rotated_geom = affinity.rotate(my_geom, -rotation, origin=origin)
-# print('rotation', rotation)
-# print(my_geom)
-# print(rotated_geom)
-# new_x = [p.x for p in rotated_geom]
-# new_y = [p.y for p in rotated_geom]
-# plt.figure()
-# plt.scatter(new_x, new_y)
-# LR1 = LinearRegression.LinearRegression()
-# new_xr = x.reshape(-1, 1)
-# LR1.fit(new_xr, new_y)
-# prediction = LR1.predict(new_xr)
-# plt.plot(prediction, y)
-# plt.show()
-# # %%
-# coords
-# rotated = rotate(my_geom, 90)
-# new_x = [p.x for p in rotated]
-# new_y = [p.y for p in rotated]
-# plt.figure()
-# plt.scatter(new_x, new_y)
-# plt.show()
-# # %%
-
-if test1 promincence 1.7 catch big stonewalls 1
-
-if test2 promincence 0.5 catch small stonewalls small
-
-if test3 rotate and find peaks caetch jorddige
-
-else  not
--- wall_score.py
+++ wall_score.py
@@ -1,32 +1,15 @@
 import sklearn.linear_model as LinearRegression
 import numpy as np
 from matplotlib import pyplot as plt
+import math
+from scipy import signal
+from shapely.geometry import MultiPoint
+from shapely import affinity
 
+## set to work for multipoint
+def linear_regression_score3D(geom):
 
-def linear_regression_score(elevs, showplots=False):
-    x = np.arange(len(elevs))
-    y = np.array(elevs)
-
-    xr = x.reshape(-1, 1)
-
-    LR = LinearRegression.LinearRegression()
-    LR.fit(xr, y)
-    score = LR.score(xr, y)
-
-   #  prediction = LR.predict(xr)
-
-   #  plt.figure()
-   #  plt.plot(x, prediction, label="linear regression", color="b")
-   #  plt.scatter(x, y, label="elevations", color="g", alpha=0.7)
-   #  plt.title(score)
-   #  plt.legend()
-   #  plt.show()
-   
-    return score
-
-def linear_regression_score3D(multipoint):
-
-    elevs = [p.z for p in multipoint]
+    elevs = [p.z for p in geom]
 
     x = np.arange(len(elevs))
     y = np.array(elevs)
@@ -36,28 +19,122 @@ def linear_regression_score3D(multipoint):
     LR = LinearRegression.LinearRegression()
     LR.fit(xr, y)
 
-    score = LR.score(xr, y)
+    # score = LR.score(xr, y)
 
     prediction = LR.predict(xr)
 
-    plt.figure()
-    plt.plot(x, prediction, label="linear regression", color="b")
-    plt.scatter(x, y, label="elevations", color="g", alpha=0.7)
-    plt.title(score)
-    plt.legend()
-    plt.show()
+    # plt.figure()
+    # plt.plot(x, prediction, label="linear regression", color="b")
+    # plt.scatter(x, y, label="elevations", color="g", alpha=0.7)
+   
+    # plt.legend()
+    # plt.show()
 
-    return score
+    slope = LR.coef_[0]
+    intercept = LR.intercept_
 
-def plot(multipoint):
-    elevs = [p.z for p in multipoint]
+    return (slope, intercept)
+## set to work for multipoint
+def only_plot(geom, title='', color='green'):
+    try:
+        elevs = [p.z for p in geom]
+    except:
+        elevs = [p.y for p in geom]
 
     x = np.arange(len(elevs))
     y = np.array(elevs)
 
     plt.figure()
-    plt.scatter(x, y, label="elevations", color="g", alpha=0.7)
+    plt.scatter(x, y, label="elevations", color=color, alpha=0.7)
+    plt.title(title)
     plt.legend()
     plt.show()
+    return True
 
+def rise_run_to_angle(rise, run):
+   return math.degrees(math.atan(rise/run))
+
+def large_stonewall_test(geom):
+    z = [p.z for p in geom]
+    peaks, _ = signal.find_peaks(z, prominence=1.7)
+    if (len(peaks) > 1): return True
+    else: return False
+
+def small_stonewall_test(geom):
+    z = [p.z for p in geom]
+    peaks, _ = signal.find_peaks(z, prominence=0.05)
+    if (len(peaks) > 1): return True
+    else: return False
+
+def earthwall_test(geom):
+    ##elevations
+    y = [p.z for p in geom]
+    ## steps in x direction
+    x = list(np.arange(0, 20.01, step=0.4))
+
+    coords = []
+    for i in range(len(y)):
+        coord = (x[i], y[i])
+        coords.append(coord)
+
+    #### calculate rotations and transformation
+    rise, intercept = linear_regression_score3D(geom)
+
+    origin = (0,intercept)
+
+    run = 0.4
+
+    rotation = rise_run_to_angle(rise, run)
+
+    new_geom = affinity.rotate(MultiPoint(coords), -rotation, origin = origin)
+
+    new_x = np.array([p.x for p in new_geom])
+    new_y = np.array([p.y for p in new_geom])
+
+    return True
+
+
+
+def no_wall_test(geom):
+    return True
+
+def pnt_from_rtn_arnd_orgn(point, origin, angle):
+
+   p = {'x': point[0], 'y':point[1]}
+   o = {'x': origin[0], 'y':origin[1]}
+
+   # if (p['x'] == o['x'] & p['y'] == o['y']): return point
+
+   cos = math.cos(angle)
+   sin = math.sin(angle)
+
+   dx = p['x'] - o['x']
+   dy = p['y'] - o['y']
+
+   nx = (cos * dx) + (sin * dy) + o['x']
+   ny = (cos * dy) - (sin * dx) + o['y']
+
+   return [nx, ny]
+
+def normalise():
+    # sbst_gdf = gdf[:500]
+
+# for _,row in sbst_gdf.iterrows():
+#    elevs = [p.z for p in row['geometry']]
+#    average = sum(elevs) / len(elevs)
+#    nrml_elevs = [(p - average) for p in elevs]
+
+#    x = np.arange(len(nrml_elevs))
+#    y = np.array(nrml_elevs)
+
+#    peaks = signal.find_peaks(y, height=0.2)
+#    print('---peaks----')
+#    print(peaks)
+
+#    plt.figure()
+#    plt.scatter(x,y)
+#    plt.yticks(np.arange(-0.6,.6, step=0.1))
+   # plt.show()
+
+# scipy.signal.find_peaks(x, height=None, threshold=None, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
     return True
