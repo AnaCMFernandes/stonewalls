21c2e9fe528e7b70adb436cd15a6568d2a28056f
ezratrotter <ezratrotter@gmail.com>
2021-02-07 17:56:18 +0100

    updated with tests

--- train_model.py
+++ train_model.py
@@ -23,36 +23,7 @@ from sklearn.utils.extmath import safe_sparse_dot
 gdf = gpd.read_file('data/3D_cross_sections.geojson')
 
 #%%
-def earthwall_test(geom):
-   ##elevations
-   z = [p.z for p in geom]
-   ## steps in x direction
-   x = list(np.arange(0, 20.01, step=0.4))
 
-   ## make x, z coords
-   coords = []
-   for i in range(len(z)):
-      coord = (x[i], z[i])
-      coords.append(coord)
-
-   wall_score.only_plot(MultiPoint(coords), 'original', 'green')
-
-   #### calculate rotations and transformation
-   rise, intercept = wall_score.linear_regression_score3D(geom)
-
-   origin = (0,intercept)
-
-   run = 0.4
-
-   rotation = wall_score.rise_run_to_angle(rise, run)
-
-   new_geom = affinity.rotate(MultiPoint(coords), -rotation, origin = origin)
-
-   wall_score.only_plot(new_geom, 'rotate', 'red')
-   # new_x = np.array([p.x for p in new_geom])
-   # new_y = np.array([p.y for p in new_geom])
-
-   return True
 
 sub_gdf = gdf[
     :200]
@@ -65,7 +36,11 @@ for _, row in sub_gdf.iterrows():
 
    obj_id = row['OBJECTID']
    geometry = row['geometry']
-   earthwall_test(geometry)
+
+   walltype = wall_score.wall_tests(geometry)
+   wall_score.only_plot(geometry, walltype)
+
+
   
 
 # %%
--- wall_score.py
+++ wall_score.py
@@ -1,3 +1,4 @@
+#%%
 import sklearn.linear_model as LinearRegression
 import numpy as np
 from matplotlib import pyplot as plt
@@ -21,7 +22,7 @@ def linear_regression_score3D(geom):
 
     # score = LR.score(xr, y)
 
-    prediction = LR.predict(xr)
+    # prediction = LR.predict(xr)
 
     # plt.figure()
     # plt.plot(x, prediction, label="linear regression", color="b")
@@ -56,14 +57,14 @@ def rise_run_to_angle(rise, run):
 
 def large_stonewall_test(geom):
     z = [p.z for p in geom]
-    peaks, _ = signal.find_peaks(z, prominence=1.7)
-    if (len(peaks) > 1): return True
+    peaks, _ = signal.find_peaks(z, prominence=0.17)
+    if (len(peaks) > 0): return True
     else: return False
 
 def small_stonewall_test(geom):
     z = [p.z for p in geom]
     peaks, _ = signal.find_peaks(z, prominence=0.05)
-    if (len(peaks) > 1): return True
+    if (len(peaks) > 0): return True
     else: return False
 
 def earthwall_test(geom):
@@ -89,11 +90,21 @@ def earthwall_test(geom):
     new_geom = affinity.rotate(MultiPoint(coords), -rotation, origin = origin)
 
     new_x = np.array([p.x for p in new_geom])
-    new_y = np.array([p.y for p in new_geom])
+    new_z = np.array([p.y for p in new_geom])
+
+    peaks, _ = signal.find_peaks(new_z, prominence=0.2)
+    if (len(peaks) > 0):
+        return True
+    else: return False
 
     return True
 
+def wall_tests(geom):
+    if large_stonewall_test(geom): return 'large_stonewall'
+    if small_stonewall_test(geom): return 'small_stonewall'
 
+    if earthwall_test(geom): return 'earthwall'
+    else: return 'no_wall'
 
 def no_wall_test(geom):
     return True
@@ -138,3 +149,46 @@ def normalise():
 
 # scipy.signal.find_peaks(x, height=None, threshold=None, distance=None, prominence=None, width=None, wlen=None, rel_height=0.5, plateau_size=None)
     return True
+
+#%%
+from osgeo import gdal, ogr
+import os
+import math
+import numpy as np
+import geopandas as gpd
+from osgeo import gdal, ogr
+import sys, math
+import numpy as np
+from shapely.geometry import Point, LineString, MultiPoint
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import LOCAL_VARS
+import make_crossline as mkcross
+import wall_score
+from scipy import signal
+from shapely import affinity
+import scipy
+from sklearn import linear_model as LinearRegression
+from sklearn.utils.extmath import safe_sparse_dot
+
+
+gdf = gpd.read_file('data/3D_cross_sections.geojson')
+
+#%%
+
+
+sub_gdf = gdf[
+    300:320]
+
+ids = []
+geometries = []
+types = []
+
+for _, row in sub_gdf.iterrows():
+
+   obj_id = row['OBJECTID']
+   geometry = row['geometry']
+
+   walltype = wall_score.wall_tests(geometry)
+   wall_score.only_plot(geometry, walltype)
+# %%
