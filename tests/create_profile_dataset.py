001d01392d42ac7a4d4b1d6f3f69c3e2d43c443b
ezratrotter <ezratrotter@gmail.com>
2021-02-09 14:28:45 +0100

    working refactored

--- tests/create_profile_dataset.py
+++ tests/create_profile_dataset.py
@@ -1,21 +1,21 @@
 #%%
 import geopandas as gpd
 import time
+import sys; sys.path.append('..'); sys.path.append('../lib')
+from lib.core import init
 
-from .context import lib
-
-path_to_stonewalls = 'data/stonewalls/aeroe/Stonewalls_AEROE'
+path_to_stonewalls = '../data/stonewalls/aeroe/Stonewalls_AEROE.shp'
 
 start = time.time()
 
 gdf = gpd.read_file(path_to_stonewalls)
 gdf = gdf.to_crs(epsg=25832)
-dtm = 'data/DTM_AEROE/DTM_AEROE.vrt'
+dtm = '../data/DTM/DTM_AEROE/DTM_AEROE.vrt'
 # sub_gdf = gdf[10:11]
 
-out_gdf = lib.init(gdf, dtm)
+out_gdf = init(gdf, dtm)
 
-out_gdf.to_file("completed_cross_sections_2.geojson", driver="GeoJSON")
+out_gdf.to_file("/data/profiles/completed_cross_sections_2.geojson", driver="GeoJSON")
 
 finish = time.time()
 
